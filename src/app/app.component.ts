a6661e428bec6e9f67a20f4564ab75918b7daf7f
Mark Thompson <mark.thompson@smpl.company>
2021-10-15 00:06:04 -0700

    woooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, TrackByFunction } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { interval, Subject } from 'rxjs';
+import { debounce, takeUntil } from 'rxjs/operators';
+
+interface ChartData {
+  value: number;
+  rank: number;
+  index: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,26 +15,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  numbers: number[] = [];
-  sum: number = 0;
-  isEven: boolean = false;
-  doubleNumbers: number[] = [];
+  chartDatas: ChartData[] = [];
+  form: FormGroup;
+  imageIndexes: number[] = [1,2,3,4,5,6];
 
-  constructor() {
-    if (window.nomodule) {
-      console.log("woo");
-    }
-    
-    console.log(this);
-    console.log(this.ngOnInit.toString());
-    console.log(JSON.stringify(this.ngOnInit));
+  private readonly unsubscriber$ = new Subject<void>();
+  
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      rankings: this.fb.array(this.imageIndexes.map(_ => [0, [Validators.min(0), Validators.max(6)]]))
+    });
   }
 
   ngOnInit(): void {
-    this.numbers = [1,3,5,7];
-    this.isEven = this.numbers.some(num => num % 2 === 0);
-    this.sum = this.numbers.reduce((sum, num) => sum + num, 0);
-    const doubleNumbers = [...this.numbers, ...this.numbers];
-    this.doubleNumbers = doubleNumbers;
+    const controls: FormControl[] = (this.form.controls.rankings as FormArray).controls as FormControl[];
+
+    this.form.controls.rankings.valueChanges.pipe(debounce(() => interval(300)),takeUntil(this.unsubscriber$)).subscribe(values => {
+      console.log('formArray', values);
+      const ranks: number[] = [...values].sort((a,b) => a - b);
+
+      this.chartDatas = (values as number[]).map((value: number, index: number) => ({
+        index: index,
+        value,
+        rank: ranks.indexOf(value),
+      }));
+    });
+
+    controls.forEach(control => control.valueChanges.pipe(takeUntil(this.unsubscriber$)).subscribe(value => {
+      console.log(value);
+      this.handleValueWasSet(controls, control);
+    }));
+  }
+
+  handleValueWasSet(controls: FormControl[], control: FormControl): void {
+    if (control.value <= 0) {
+      return;
+    }
+
+    controls.forEach(otherControl => {
+      if (otherControl === control) {
+        return;
+      }
+
+      if (otherControl.value === control.value) {
+        otherControl.setValue(Math.max(0, control.value - 1));
+        this.handleValueWasSet(controls, otherControl);
+      }
+    })
+  }
+
+  chartDataTrackBy(index: number, chartdata: ChartData): number {
+    return chartdata.index;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber$.next();
+    this.unsubscriber$.complete();
   }
 }
